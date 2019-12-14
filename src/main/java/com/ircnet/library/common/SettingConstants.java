66e71369bcaec1999fcef4b39b04cd39346ddf59
ircnet-com <53142088+ircnet-com@users.noreply.github.com>
2019-12-14 14:18:48 +0100

    Update SettingConstants.java

--- src/main/java/com/ircnet/library/common/SettingConstants.java
+++ src/main/java/com/ircnet/library/common/SettingConstants.java
@@ -2,10 +2,10 @@ package com.ircnet.library.common;
 
 public class SettingConstants {
     public static final String CONNECT_TIMEOUT = "connect-timeout";
-    public static final int CONNECT_TIMEOUT_DEFAULT = 30000;
+    public static final int CONNECT_TIMEOUT_DEFAULT = 300;
 
     public static final String REGISTRATION_TIMEOUT = "registration-timeout";
-    public static final int REGISTRATION_TIMEOUT_DEFAULT = 30000;
+    public static final int REGISTRATION_TIMEOUT_DEFAULT = 300;
 
     public static final String RECONNECT_DELAY = "reconnect-delay";
     public static final int RECONNECT_DELAY_DEFAULT = 20;
@@ -14,5 +14,5 @@ public class SettingConstants {
     public static final int LAGCHECK_INTERVAL_DEFAULT = 0;
 
     public static final String MAX_LAG_BEFORE_DISCONNECT = "max-lag-before-disconnect";
-    public static final int MAX_LAG_BEFORE_DISCONNECT_DEFAULT = 30000;
+    public static final int MAX_LAG_BEFORE_DISCONNECT_DEFAULT = 300;
 }
