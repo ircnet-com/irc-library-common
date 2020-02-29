40d9ad720568332c0f057f4686b48ee63fad18ab
Patrick <patrick@ircnet.com>
2020-02-29 18:31:42 +0100

    Refactoring

--- src/main/java/com/ircnet/library/common/Client.java
+++
@@ -1,9 +0,0 @@
-package com.ircnet.library.common;
-
-import com.ircnet.library.common.configuration.ConfigurationModel;
-import com.ircnet.library.common.connection.IRCConnection;
-
-public interface Client<S extends IRCConnection, T extends ConfigurationModel> {
-    S getIRCConnection();
-    T getConfiguration();
-}
