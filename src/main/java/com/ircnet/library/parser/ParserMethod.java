c1bc5c88cc957510391731a1c7bfeb27342375e3
Patrick <patrick@ircnet.com>
2022-01-13 15:49:33 +0100

    Preparation for IRCv3 message tags

--- src/main/java/com/ircnet/library/parser/ParserMethod.java
+++ src/main/java/com/ircnet/library/parser/ParserMethod.java
@@ -2,7 +2,9 @@ package com.ircnet.library.parser;
 
 import com.ircnet.library.common.connection.IRCConnection;
 
+import java.util.Map;
+
 @FunctionalInterface
 public interface ParserMethod<T extends IRCConnection> {
-    void parse(T ircConnection, String[] parts);
+    void parse(T ircConnection, String[] parts, Map<String, String> messageTags);
 }
