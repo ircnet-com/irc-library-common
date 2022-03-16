2bf038a4921aaf47286a598737a420e2a94fdb63
Patrick <patrick@ircnet.com>
2022-03-16 20:16:24 +0100

    Log ERROR message

--- src/main/java/com/ircnet/library/parser/ParserImpl.java
+++ src/main/java/com/ircnet/library/parser/ParserImpl.java
@@ -1,7 +1,10 @@
 package com.ircnet.library.parser;
 
+import com.ircnet.library.common.Util;
 import com.ircnet.library.common.connection.IRCConnection;
 import com.ircnet.library.common.connection.IRCConnectionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
@@ -9,6 +12,8 @@ import java.util.HashMap;
 import java.util.List;
 
 public class ParserImpl<T extends IRCConnection> implements Parser<T> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ParserImpl.class);
+
     @Autowired
     private IRCConnectionService ircConnectionService;
 
@@ -18,6 +23,7 @@ public class ParserImpl<T extends IRCConnection> implements Parser<T> {
         parserMappingList = new ArrayList<>();
         parserMappingList.add(new ParserMapping<>("PING", 0, 2, (arg1, arg2, arg3) -> parsePing(arg1, arg2)));
         parserMappingList.add(new ParserMapping<>("PONG", 1, 4, (arg1, arg2, arg3) -> parsePong(arg1, arg2)));
+        parserMappingList.add(new ParserMapping<>("ERROR", 0, 2, (arg1, arg2, arg3) -> parseError(arg1, arg2)));
     }
 
     @Override
@@ -38,7 +44,10 @@ public class ParserImpl<T extends IRCConnection> implements Parser<T> {
     }
 
     protected void parsePong(T ircConnection, String[] parts) {
-        // :fu-berlin.de PONG fu-berlin.de :foobar
         ircConnectionService.handleLagCheckResponse(ircConnection, parts[3].substring(1));
+    }
+
+    private void parseError(T ircConnection, String[] parts) {
+        LOGGER.info("Received error: {}", Util.removeLeadingColon(parts[1]));
     }
 }
