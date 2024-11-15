e9be5d0c67273fe9e3b67e18dc635168a14fce3a
Patrick <patrick@ircnet.com>
2024-11-15 10:13:19 +0100

    Refactoring

--- src/main/java/com/ircnet/library/common/event/AbstractEventListener.java
+++ src/main/java/com/ircnet/library/common/event/AbstractEventListener.java
@@ -5,13 +5,14 @@ import com.ircnet.library.common.connection.IRCConnectionService;
 import java.lang.reflect.ParameterizedType;
 import java.util.EventListener;
 
-public abstract class AbstractEventListener<T extends AbstractEvent> implements EventListener {
-    protected IRCConnectionService ircConnectionService;
+public abstract class AbstractEventListener<T extends AbstractEvent, T2 extends IRCConnectionService> implements EventListener {
+    protected T2 ircConnectionService;
 
     private String owner;
     private Class<T> eventClass;
 
-    public AbstractEventListener() {
+    public AbstractEventListener(T2 ircConnectionService) {
+        this.ircConnectionService = ircConnectionService;
         this.eventClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
     }
 
