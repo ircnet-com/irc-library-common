6453d2b93a609cb448a18d5ddaa62a2166189827
Patrick <patrick@ircnet.com>
2025-03-09 22:39:23 +0100

    Merged v1.1

--- src/main/java/com/ircnet/library/common/IRCTaskService.java
+++
@@ -1,5 +0,0 @@
-package com.ircnet.library.common;
-
-public interface IRCTaskService {
-    void run(IRCTask ircTask);
-}
--- src/main/java/com/ircnet/library/parser/ParserMethod.java
+++
@@ -1,8 +0,0 @@
-package com.ircnet.library.parser;
-
-import com.ircnet.library.common.connection.IRCConnection;
-
-@FunctionalInterface
-public interface ParserMethod<T extends IRCConnection> {
-    void parse(T ircConnection, String[] parts);
-}
